7470248a3586ae0c4d526b5f9dbb1573aeaec9c5
jkudo <kudo@DESKTOP-0N1SEQ2.localdomain>
2022-07-29 01:52:35 +0900

    vm-simple-script

---
+++ vm-simple-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+REGION="eastus"
+RGPREFIX="_group"
+IMAGE="UbuntuLTS"
+USERNAME="azureuser"
+PASSWORD="xxxxxxxxxx"
+SIZE="Standard_B1ms"
+
+if [ "$1" = "create" ]; then
+	az group create --name $2$RGPREFIX --location $REGION
+	az vm create --resource-group $2$RGPREFIX --name $2 --image $IMAGE --admin-username $USERNAME --admin-password $PASSWORD --size $SIZE
+elif [ "$1" = "delete" ]; then
+	az group delete --name $2$RGPREFIX --force-deletion-types Microsoft.Compute/virtualMachines --yes
+elif [ "$1" = "list" ]; then
+        az vm list --output table
+else
+	echo "Can not find option."
+	echo "create: vm create"
+        echo "delete: vm delete"
+	echo "list: vm list"
+fi
